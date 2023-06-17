d73ca759ba2907b92990b92e171ca79528a9c5e7
John Abiodun <abiodunjohn513@gmail.com>
2023-06-17 13:03:46 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line. "
- * return 0.
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: Alwways  0.
  */
 
 int main(void)
@@ -11,7 +11,6 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+        return (0);
 
 }
