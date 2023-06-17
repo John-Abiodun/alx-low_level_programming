feac2a82aed1eb41d404f48ff1b0e87d4fa4a2b7
John Abiodun <abiodunjohn513@gmail.com>
2023-06-17 15:04:52 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,14 @@
 
 
 /**
- * main - prints "the size of various types based on the computer it is compiled and run on..."
+ * main - prints "the size of various types based on the computer it is compiled and run on"
  * Return: Always 0.
  *
  */
 
 int main(void)
         
+
 {       
         printf("Size of a char: %zu byte(s)\n", sizeof(char));
         
