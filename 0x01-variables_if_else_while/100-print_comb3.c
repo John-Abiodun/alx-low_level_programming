78b26773347ed2be506ff7e097c3bbe45919c61a
John Abiodun <abiodunjohn513@gmail.com>
2023-06-19 20:13:08 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main -  prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int f, g;
+
+	for (f = '0'; f < '9'; f++)
+	{
+	for ( g = f + 1; g <= '9'; g++)
+	{
+	if (g != f)
+	{
+	putchar(f);
+	putchar(g);
+	if (f == '8' && g == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+
+}
