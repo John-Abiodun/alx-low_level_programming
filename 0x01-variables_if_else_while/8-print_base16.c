34f5a85b104d2612656151fa60e816daace48543
John Abiodun <abiodunjohn513@gmail.com>
2023-06-19 19:22:38 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - prints base sixteen numbers in lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int a;
+	char low;
+
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
