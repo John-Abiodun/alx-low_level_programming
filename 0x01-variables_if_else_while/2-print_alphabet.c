93f3d7ea3c1bb43e651ccf4da824751883f86aa6
John Abiodun <abiodunjohn513@gmail.com>
2023-06-19 17:48:01 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main Prints alphabet in lower case
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
