7e0c9a450e5b3e0a3f5e7f787a697d2a8f4d4d17
John Abiodun <abiodunjohn513@gmail.com>
2023-06-19 18:26:14 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Prints single digit base numbers of base ten
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int s;
+
+	for (s = '0'; s <= '9'; s++)
+	putchar(s);
+	putchar('\n');
+
+	return (0);
+
+}
