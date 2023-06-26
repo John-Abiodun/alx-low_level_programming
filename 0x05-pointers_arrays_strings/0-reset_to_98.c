5639a3dac146cf4fadb9aaf0b427366f5b004ce0
John Abiodun <abiodunjohn513@gmail.com>
2023-06-26 08:53:55 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a point to an int and the updates the value
+ *
+ * @n: an integer input by the point
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
