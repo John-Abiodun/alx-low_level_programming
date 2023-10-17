e31c0b677af3a38800d8c5f7dc315e56a5d94578
John Abiodun <abiodunjohn513@gmail.com>
2023-10-16 19:42:12 -0700

    Task 2 & 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two args numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
+int main(int argc, char *argv[])
+{
+	int b1 = 0, b2 = 0;
+
+	if (argc == 3)
+	{
+		b1 = atoi(argv[1]);
+		b2 = atoi(argv[2]);
+		printf("%d\n", b1 * b2);
+	
+	}
+	else
+	{
+		printf("Error);
+		return (1);
+	}
+
+	return (0);
+}
