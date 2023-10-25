9a073a002f9386c83a8fbe326fe8b11277d44396
John Abiodun <abiodunjohn513@gmail.com>
2023-10-24 23:00:27 -0700

    task 4.1

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
 	for (index = 0; index < bytes; index++)
 	{
-		opcode = *(unsinged char *)address;
+		opcode = *(unsigned char *)address;
 		printf("%.2x", opcode);
 
 		if (index == bytes - 1)
