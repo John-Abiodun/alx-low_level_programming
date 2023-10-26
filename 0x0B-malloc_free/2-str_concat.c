d6693c26e7dbf7a859b551db4df572f44ee09074
John Abiodun <abiodunjohn513@gmail.com>
2023-10-25 21:39:20 -0700

    correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,11 +20,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[1])
+	while (s1[i])
 		i++;
 
 	while (s2[j])
 		j++;
+
+	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 
 	if (s == NULL)
