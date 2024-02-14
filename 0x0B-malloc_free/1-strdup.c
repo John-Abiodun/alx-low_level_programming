a7a0045cea3e8db2566b5bb7bf596d9d073c8b05
John Abiodun <abiodunjohn513@gmail.com>
2024-02-14 16:00:34 +0100

    Task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate a string
+ * @str: string to duplicate
+ *
+ * Return: string duplicated
+ */
+
+char *_strdup(char *str)
+{
+	int a = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	s = malloc((sizeof(char) * i) + );
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+
+	s[a] = '\0';
+	return (s);
+}
